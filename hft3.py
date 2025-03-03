409ca32c57160497cd3cdce4e812783ff5a0e340
JustCoddingg <gameofahmetgokhan@gmail.com>
2025-03-03 13:45:59 +0300

    hft3.py

--- hft3.py
+++ hft3.py
@@ -1,1 +1,12 @@
 # hft3
+print('Galatasaray gol sayisi:')
+g = int(input())  # Kullanıcıdan gelen değeri tam sayıya çeviriyoruz
+print('Fenerbahçe gol sayisi:')
+f = int(input())
+
+if g > f:
+    print('Galatasaray kazandı')
+elif g == f:
+    print('Berabere')
+else:
+    print('Fenerbahçe kazandı.')
